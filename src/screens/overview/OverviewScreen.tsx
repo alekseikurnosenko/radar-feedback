0aefd8ff2083c6808d2c2ed9d3238508f829b3cd
Aleksey Kurnosenko <lekz112@gmail.com>
2020-05-08 12:07:26 +0200

    Extract AnswersOverview component

--- src/screens/overview/OverviewScreen.tsx
+++ src/screens/overview/OverviewScreen.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import getUserAnswers from "./getUserAnswers";
 import { getUserId } from "./getUserId";
-import getMeasurements from "../questionnaire/getMeasurements";
+import getMeasurements, { Measurement } from "../questionnaire/getMeasurements";
 import getQuestions from "../questionnaire/getQuestions";
 import { RadarChart } from "../../components/RadarChart";
 import { Link, useHistory, Redirect } from "react-router-dom";
@@ -9,14 +9,67 @@ import firebase from "firebase";
 import { UserAnswers } from "../questionnaire/saveUserAnswers";
 import startSession from "../session/startSession";
 
+const AnswersOverview = (props: { measurements: Measurement[], answers: UserAnswers[] }) => {
+    const { measurements, answers } = props;
+    const questions = getQuestions();
+
+    const currentAnswers = answers[0];
+    const previousAnswers = answers[1];
+    return (
+        <div className="flex flex-1 flex-col px-8">
+            {measurements.map(m => {
+                const currentValue = Object.values(currentAnswers).flatMap(answers => answers).reduce((sum, answer) => sum + (answer.measurement === m ? answer.value : 0), 0)
+                const previousValue = Object.values(previousAnswers || {}).flatMap(answers => answers).reduce((sum, answer) => sum + (answer.measurement === m ? answer.value : 0), 0)
+
+                const diff = previousAnswers && (currentValue - previousValue);
+                return (
+                    <div className="flex flex-col" key={m}>
+                        <div className="flex flex-row items-center">
+                            <p className="text-2xl mr-2">{m} {currentValue}/5</p>
+                            {diff !== undefined && (
+                                diff > 0 ? <p className="text-xl text-green-500">▲ {diff}</p> :
+                                    diff < 0 ? <p className="text-xl text-red-800">▼ {Math.abs(diff)}</p> : null
+                            )
+                            }
+                        </div>
+                        {
+                            questions && Object.values(currentAnswers)
+                                .flatMap(answers => answers)
+                                .filter(a => a.measurement === m)
+                                .map(a => {
+                                    const question = questions.find(q => q.answers.some(aa => aa.id === a.id));
+
+                                    const origAnswer = question?.answers.find(aa => aa.id === a.id);
+
+                                    return (
+                                        <div className="flex flex-col pl-8 mb-4" key={a.id}>
+                                            <p className="italic mb-2 text">{question?.text}</p>
+                                            <p className="p-2 mb-2 border border-sold border-black rounded-lg">✔️ {a.text}</p>
+                                            {origAnswer?.suggestions && origAnswer?.suggestions.map(s => (
+                                                <div className="pl-2 flex flex-row items-center">
+                                                    <p>⚠ {s.text}</p>
+                                                    {s.link && <a className="ml-2 underline text-blue-500" target="_blank" href={s.link}>Link</a>}
+                                                </div>
+                                            ))}
+                                        </div>
+                                    )
+                                })
+                        }
+                    </div>
+                )
+            })
+            }
+        </div>
+    )
+};
+
 export const OverviewScreen = () => {
     const userId = getUserId()!;
     const measurements = getMeasurements();
     const [answers, isAnswersLoading] = getUserAnswers(userId);
-    const questions = getQuestions();
     const history = useHistory();
 
-    if (!measurements || !answers || !questions) {
+    if (!measurements || !answers) {
         return <p>Loading</p>;
     }
 
@@ -48,50 +101,7 @@ export const OverviewScreen = () => {
             </div>
             <div className="flex lg:flex-row flex-col">
                 <div className="flex flex-1 flex-col">
-                    <div className="flex flex-1 flex-col px-8">
-                        {measurements.map(m => {
-                            const currentValue = Object.values(currentAnswers).flatMap(answers => answers).reduce((sum, answer) => sum + (answer.measurement === m ? answer.value : 0), 0)
-                            const previousValue = Object.values(previousAnswers || {}).flatMap(answers => answers).reduce((sum, answer) => sum + (answer.measurement === m ? answer.value : 0), 0)
-
-                            const diff = previousAnswers && (currentValue - previousValue);
-                            return (
-                                <div className="flex flex-col" key={m}>
-                                    <div className="flex flex-row items-center">
-                                        <p className="text-2xl mr-2">{m} {currentValue}/5</p>
-                                        {diff !== undefined && (
-                                            diff > 0 ? <p className="text-xl text-green-500">▲ {diff}</p> :
-                                                diff < 0 ? <p className="text-xl text-red-800">▼ {Math.abs(diff)}</p> : null
-                                        )
-                                        }
-                                    </div>
-                                    {
-                                        Object.values(currentAnswers)
-                                            .flatMap(answers => answers)
-                                            .filter(a => a.measurement === m)
-                                            .map(a => {
-                                                const question = questions.find(q => q.answers.some(aa => aa.id === a.id));
-
-                                                const origAnswer = question?.answers.find(aa => aa.id === a.id);
-
-                                                return (
-                                                    <div className="flex flex-col pl-8 mb-4" key={a.id}>
-                                                        <p className="italic mb-2 text">{question?.text}</p>
-                                                        <p className="p-2 mb-2 border border-sold border-black rounded-lg">✔️ {a.text}</p>
-                                                        {origAnswer?.suggestions && origAnswer?.suggestions.map(s => (
-                                                            <div className="pl-2 flex flex-row items-center">
-                                                                <p>⚠ {s.text}</p>
-                                                                {s.link && <a className="ml-2 underline text-blue-500" target="_blank" href={s.link}>Link</a>}
-                                                            </div>
-                                                        ))}
-                                                    </div>
-                                                )
-                                            })
-                                    }
-                                </div>
-                            )
-                        })
-                        }
-                    </div>
+                    <AnswersOverview answers={answers.map(a => a.answers)} measurements={measurements} />
                 </div>
                 <div className="m-16">
                     {measurements && <RadarChart
